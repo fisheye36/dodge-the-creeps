a6d38627104ea9d7e891f83d624aaee9bc103f73
Kamil Warchoł <kamil.warchol.93@gmail.com>
2019-03-31 23:26:17 +0200

    Animate enemies when instantiated

--- scenes/Mob/Mob.gd
+++ scenes/Mob/Mob.gd
@@ -9,12 +9,12 @@ func get_randomized_speed() -> int:
     return rand_range(min_speed, max_speed) as int
 
 func _ready() -> void:
-    _select_random_animation()
+    _play_random_animation()
 
-func _select_random_animation() -> void:
+func _play_random_animation() -> void:
     var mob_sprite := $MobAnimatedSprite as AnimatedSprite
     var animation_names := mob_sprite.frames.get_animation_names()
-    mob_sprite.animation = animation_names[randi() % animation_names.size()]
+    mob_sprite.play(animation_names[randi() % animation_names.size()])
 
 func _on_screen_exited() -> void:
     queue_free()
